f38b380f440b0fd9d2b6a2be6948c3d3f334cb5c
ogane0112 <kai513272@gmail.com>
2024-02-22 04:06:45 +0900

    perfect

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -17,18 +17,16 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
+              Hello!
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "My Name is",
                 1000,
-                "Web Developer",
+                "Kaito",
                 1000,
-                "Mobile Developer",
-                1000,
-                "UI/UX Designer",
+                "Ogane",
                 1000,
               ]}
               wrapper="span"
@@ -37,8 +35,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+           ↓から個人ブログやCONTACTにアクセスすることができます！
           </p>
           <div>
             <Link
@@ -48,7 +45,7 @@ const HeroSection = () => {
               CONTACT
             </Link>
             <Link
-              href="/"
+              href="https://note.com/gifted_nerine416"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
@@ -63,13 +60,13 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full  w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/hero-image.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={270}
-              height={270}
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full "
+              width={350}
+              height={350}
             />
           </div>
         </motion.div>
