667cdd2e7cba4a45917a9da0701c19253d2064dd
ogane0112 <kai513272@gmail.com>
2024-02-22 17:00:31 +0900

    delete nav ultag

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -57,12 +57,12 @@ const Navbar = () => {
         </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-            <li><Link href="#about">about</Link></li>
+            <Link href="#about">about</Link>
 
             {navLinks.map((link, index) => (
-              <li key={index}>
+  
                 <NavLink href={link.path} title={link.title} />
-              </li>
+              
             ))}
           </ul>
         </div>
