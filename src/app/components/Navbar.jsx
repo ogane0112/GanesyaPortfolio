bda85f0134101c5ce6f4c08fe9ab7fcea31cae1a
ogane0112 <kai513272@gmail.com>
2024-02-22 17:32:15 +0900

    delete about

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -57,7 +57,6 @@ const Navbar = () => {
         </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-            <Link href="#about">about</Link>
 
             {navLinks.map((link, index) => (
               <li key={index}>
